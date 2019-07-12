2263a8ba67f699723e8f91473c46af94400a8b8e
FerSO7 <fernando.ochoa.mza@gmail.com>
2019-07-12 16:44:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/mislugares/AcercaDeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mislugares
+
+import android.app.Activity
+import android.os.Bundle
+
+
+class AcercaDeActivity: Activity() {
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.acerca_de)
+
+    }
+
+}
