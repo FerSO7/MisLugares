ee125a02cfeb8f2d3d548920068505b3b4d111a0
FerSO7 <fernando.ochoa.mza@gmail.com>
2019-07-18 18:17:43 -0300

    16/7

--- app/src/main/java/com/example/mislugares/MainActivity.kt
+++ app/src/main/java/com/example/mislugares/MainActivity.kt
@@ -30,12 +30,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
         return when (item.itemId) {
             R.id.action_settings -> true
+            R.id.acercaDe -> {
+               val intent = Intent(this,AcercaDeActivity :: class.java)
+                startActivity(intent)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
